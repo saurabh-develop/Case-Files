844cef3ec24c48f94f0e22489e22f89e4b1b7d2c
mrstmcpp <mrstm.cpp@gmail.com>
2024-03-09 07:57:37 +0530

    api creation on progress

--- client/service/api.js
+++ client/service/api.js
@@ -7,66 +7,66 @@ const axiosInstance = axios.create({
     baseURL: API_URL,
     timeout: 10000,
     headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json"
     }
 })
 
 axiosInstance.interceptors.request.use(
-    function(config){
+    function (config) {
         return config;
 
     },
-    function(error){
+    function (error) {
         return Promise.reject(error);
 
     }
 )
 
 axiosInstance.interceptors.response.use(
-    function(response){
+    function (response) {
         //stop globbal loader 
         return processResponse(response);
     },
-    function(error){
+    function (error) {
         return Promise.reject(processError(error));
     }
 )
 
-const processResponse = (response) =>{
-     if(response?.status === 200){
-        return { isSuccess:true , data: response.data }
+const processResponse = (response) => {
+    if (response?.status === 200) {
+        return { isSuccess: true, data: response.data }
 
-     } else{
+    } else {
         return {
             isFailure: true,
             status: response?.status,
             msg: response?.msg,
             code: response?.code
         }
-     }
+    }
 }
 
 const processError = (error) => {
-    if(error.response){
+    if (error.response) {
         //responded with code other than 200
-        console.log('Error in response: ' , error.toJSON());
-        return{
+        console.log('Error in response: ', error.toJSON());
+        return {
             isError: true,
             msg: API_Defaults.responseFailure,
             code: error.response.status
         }
-    } else if(error.request){
+    } else if (error.request) {
         //request made bt no response == check connectivity of back nd front
-        console.log('Error in request: ' , error.toJSON());
-        return{
+        console.log('Error in request: ', error.toJSON());
+        return {
             isError: true,
             msg: API_Defaults.requestFailure,
             code: ""
         }
-    } else{
+    } else {
         //frontend error
-        console.log('Error in network : ' , error.toJSON());
-        return{
+        console.log('Error in network : ', error.toJSON());
+        return {
             isError: true,
             msg: API_Defaults.networkError,
             code: ""
@@ -77,5 +77,30 @@ const processError = (error) => {
 
 
 const API = {
-    
+
+};
+
+for (const [key, value] of Object.entries(SERVICE_URLS)) {
+    API[key] = (body, showUploadProgress, showDownloadProgress) =>
+        axiosInstance({
+            method: value.method,
+            url: value.url,
+            data: body,
+            responseType: value.responseType,
+            onUploadProgress: function (progressEvent) {
+                if (showUploadProgress) {
+                    let percentageCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+                    showUploadProgress(percentageCompleted);
+                }
+            },
+            onDownloadProgress: function (progressEvent) {
+                if (showDownloadProgress) {
+                    let percentageCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+                    showDownloadProgress(percentageCompleted);
+                }
+            }
+        })
 }
+
+
+export { API };
