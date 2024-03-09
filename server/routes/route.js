07cbc6c86a6a46e9fa8be02c05ac2e113fa2954f
bhujo <utkarshapandey2004@gmail.com>
2024-03-09 15:25:20 +0530

    login-backend

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,9 @@
 import  express  from "express";
-import { signupUser } from "../controller/usercontol.js";
+import { signupUser , loginUser} from "../controller/usercontol.js";
 
 const router = express.Router();
 
 router.post('/signup', signupUser);
+router.post('/login',loginUser);
 
 export default router;
