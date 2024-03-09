570db2f928a9049b3bb57cc9a9ffc11c7c90a6af
bhujo <utkarshapandey2004@gmail.com>
2024-03-09 11:09:30 +0530

    .

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import {signupUser2} from "/Projects/Case-Files/server/controller/usercontol.js";
+// import {signupUser2} from "/Projects/Case-Files/server/controller/usercontol.js";
 import { signupUser } from "../controller/usercontol.js";
 
 const router = express.Router();
