a729b292539a9acdde7729717a85b346f9db48a6
mrstmcpp <mrstm.cpp@gmail.com>
2024-03-09 11:42:59 +0530

    some minor bugs fixed

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,4 @@
 import  express  from "express";
-// import {signupUser2} from "/Projects/Case-Files/server/controller/usercontol.js";
 import { signupUser } from "../controller/usercontol.js";
 
 const router = express.Router();
