fa71710930b87c8af285a0093ee6c857273a75ee
mrstmcpp <mrstm.cpp@gmail.com>
2024-03-10 12:33:18 +0530

    minor errors fixed

--- server/controller/usercontol.js
+++ server/controller/usercontol.js
@@ -1,5 +1,10 @@
 import bcrypt from 'bcrypt';
 import User from '../model/user.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import Token from '../model/token.js';
+
+dotenv.config();
 
 export const signupUser = async (request,response) =>{
     try{
@@ -18,5 +23,25 @@ export const signupUser = async (request,response) =>{
     
 };
 
+export const loginUser = async(request,response) =>{
+    let user = await User.findOne({username:request.body.username});
+    if ( !user){
+        return response.status(400).json({msg:'Username does not match'});
+    }
+    try {
+        let match=await bcrypt.compare(request.body.password,user.password);
+        if (match){
+              const accessToken= jwt.sign(user.toJSON(),process.env.ACCESS_SECRET_KEY,{expiresIn:'15m'});
+              const refreshToken=jwt.sign(user.toJSON(),process.env.REFRESH_SECRET_KEY);
 
+              const newToken =new Token({token:refreshToken})
+              await newToken.save();
 
+              return response.status(200).json({accessToken:accessToken,refreshToken:refreshToken,name:user.name,username:user.username})
+        }else{
+           return response.status(400).json({msg:'Password does not match'})
+        }
+    } catch (error) {
+        return response.status(500).json({msg:'Error while login in user'})
+    }
+}
