56a75e1bef72aac9f605d69ea02dcf819fffc81b
bhujo <utkarshapandey2004@gmail.com>
2024-03-11 11:20:51 +0530

    file upload feature backend

---
+++ server/controller/image-controller.js
@@ -0,0 +1,13 @@
+const url = 'http://localhost:8000';
+
+export const uploadImage=(request,response)=>{
+    if(!request.file){
+        return response.status(404).json({
+            msg:"File not found"
+        })
+    }
+
+        const imageUrl = `${url}/file/${request.file.filename}`;
+
+        return response.status(200).json(imageUrl);
+    }
