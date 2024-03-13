2c5716f27dcbc42c5d2a385d5712ce86e5f7796d
new <new@kali.dell.com>
2024-03-13 18:13:20 +0530

    category

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -1,31 +1,55 @@
-import {Box,Typography} from '@mui/material';
-import {useParams } from 'react-router-dom';
-import {useEffect,useState} from 'react';
-import {API} from '../../service/api';
- 
- const DetailView=()=>{
+import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { API } from "../../service/api";
+import styled from "@emotion/styled";
 
-    const[post,setPost] = useState({});
+const Container = styled(Box)`
+  margin: 92px 100px 50px 100px;
+`;
 
-    const { id } = useParams();
-    const url='https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
+const Image = styled("img")({
+  width: "100%",
+  height: "50vh",
+  objectFit: "cover",
+});
 
-    useEffect(()=>{
-        const fetchData = async() =>{
-           let response = await API.getPostById(id);
-           if(response.isSuccess){
-            setPost(response.data);
-           }
-        }
-        fetchData();
+const Heading = styled(Typography)`
+  font-size: 38px;
+  font-weight: 600;
+  text-align: center;
+  margin: 10px 0px 10px 0px;
+`;
 
-    },[])
+const DetailView = () => {
+  const [post, setPost] = useState({});
 
-    return(
-        <Box>
-           <img src={url} alt="blog" />
-        </Box>
-    )
-}
+  const { id } = useParams();
+  const url = post.picture
+    ? post.picture
+    : "https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80";
 
-export default DetailView; 
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await API.getPostById(id);
+      if (response.isSuccess) {
+        setPost(response.data);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Image src={url} alt="blog" />
+      <Heading>{post.title}</Heading>
+      <Box>
+        <Typography>{post.username}</Typography>
+        <Typography>{new Date(post.createdDate).toDateString()}</Typography>
+      </Box>
+      <Typography>{post.description}</Typography>
+    </Container>
+  );
+};
+
+export default DetailView;
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,21 +1,24 @@
-import  express  from "express";
-import { signupUser , loginUser} from "../controller/usercontol.js";
-import { uploadImage,getImage } from "../controller/image-controller.js";
+import express from "express";
+import { signupUser, loginUser } from "../controller/usercontol.js";
+import { uploadImage, getImage } from "../controller/image-controller.js";
 import { authenticateToken } from "../controller/jwt-controller.js";
-import {createPost,getAllPosts,getPost} from "../controller/post-controller.js";
+import {
+  createPost,
+  getAllPosts,
+  getPost,
+} from "../controller/post-controller.js";
 
-import upload from '../utils/upload.js'
+import upload from "../utils/upload.js";
 const router = express.Router();
 
-router.post('/signup', signupUser);
-router.post('/login',loginUser);
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
-router.post('/file/upload',upload.single('file'),uploadImage);
-router.get('file/:filename',getImage);
+router.post("/file/upload", upload.single("file"), uploadImage);
+router.get("file/:filename", getImage);
 
-router.post('/create',authenticateToken,createPost);
-router.get('/posts',authenticateToken,getAllPosts);
-router.get('/post:id',authenticateToken,getPost);
-
+router.post("/create", authenticateToken, createPost);
+router.get("/posts", authenticateToken, getAllPosts);
+router.get("/post/:id", authenticateToken, getPost);
 
 export default router;
